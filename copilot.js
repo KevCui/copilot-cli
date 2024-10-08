eae31057bb8fd3736509114e5fe2fed50f5e2b48
Kevin Cui <krazycavin@gmail.com>
2024-10-09 01:07:12 +0200

    Update script due to UI changes

--- copilot.js
+++ copilot.js
@@ -6,13 +6,12 @@ chromium.use(stealth);
 
 const searchText = process.argv[2];
 const url = 'https://copilot.microsoft.com';
-const buttonSubmit = '.submit';
-const buttonReject = '.bnp_btn_reject';
-const buttonStop = '#stop-responding-button';
-const textMessage = '.ac-container';
-const textareaSearchBox = '#searchbox';
+const buttonStart = '[title="Get started"]';
+const buttonSubmit = '[title="Submit message"]';
+const textMessage = '[data-content="ai-message"] div';
+const textareaSearchBox = '#userInput';
 const totalLoopCount = 60;
-const printIntervalTime = 500;
+const printIntervalTime = 800;
 
 chromium.launch({ headless: true, timeout: 30000 }).then(async browser => {
   // Set page
@@ -29,24 +28,27 @@ chromium.launch({ headless: true, timeout: 30000 }).then(async browser => {
   // Start page
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-  // Reject cookie
-  await page.click(buttonReject);
+  // Click "Get start" button
+  if (await page.isVisible(buttonStart)) {
+    await page.click(buttonStart);
+  }
 
   // Submit question
   await page.fill(textareaSearchBox, searchText);
   await page.click(buttonSubmit);
 
-
   // Get reply
+  var prevResult = '';
   for (let i = 0; i < totalLoopCount; i++) {
     await new Promise((resolve) => setTimeout(resolve, printIntervalTime));
-    const result = await page.locator(textMessage).textContent();
+    const result = await page.locator(textMessage).first().innerText();
     console.clear();
     console.log('----------\n' + result.replace(/^\s*\n+/gm, '\n'));
-    if (await page.locator(buttonStop).isDisabled()
-      && i != (totalLoopCount - 1)){
+    if (prevResult == result 
+      && i != (totalLoopCount - 1)) {
       i = (totalLoopCount - 1);
     }
+    prevResult = result
   }
 
   // Close browser
