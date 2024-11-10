f974c9fea8ea83fa89ac608c69d003f8834726d2
Kevin Cui <krazycavin@gmail.com>
2024-11-10 20:00:40 +0100

    Reformat and clean up output

--- copilot.js
+++ copilot.js
@@ -32,20 +32,20 @@ chromium.launch({ headless: true, timeout: 30000 }).then(async browser => {
 
   // Click "Get start" button
   try {
-    await page.locator(buttonStart).waitFor({ timeout: 1000})
+    await page.locator(buttonStart).waitFor({ timeout: 2500})
     await page.click(buttonStart);
   } catch (e) {}
 
   // Fill first name
   try { 
-    await page.locator(textFirstName).waitFor({ timeout: 1000})
+    await page.locator(textFirstName).waitFor({ timeout: 800})
     await page.fill(textFirstName, 'x');
     await page.click(buttonSubmit);
   } catch (e) {}
 
   // Click "Next" button
   try { 
-    await page.locator(buttonNext).waitFor({ timeout: 1000})
+    await page.locator(buttonNext).waitFor({ timeout: 800})
     await page.click(buttonNext);
   } catch (e) {}
 
@@ -57,10 +57,23 @@ chromium.launch({ headless: true, timeout: 30000 }).then(async browser => {
   var prevResult = '';
   for (let i = 0; i < totalLoopCount; i++) {
     await new Promise((resolve) => setTimeout(resolve, printIntervalTime));
-    const result = await page.locator(textMessage).first().innerText();
+    const elems = await page.locator(textMessage)
+    const count = await elems.count();
+    var result = '';
+    for (let j = 0; j < count; j++) {
+      result += await elems.nth(j).innerHTML();
+    }
+    // remove buttons
+    result = result.replace(/<button .*?>.*?<\/button>/g, "");
+    // convert <p> to newline
+    result = result.replace(/<p>(.*?)<\/p>/g, '\n$1\n');
+    // remove remaining HTML tags
+    result = result.replace(/<\/?[^>]+(>|$)/g, "");
+
     console.clear();
-    console.log('----------\n' + result.replace(/^\s*\n+/gm, '\n'));
-    if (prevResult == result && i != (totalLoopCount - 1)) {
+    console.log('----------\n' + result);
+
+    if (prevResult != '' && prevResult == result && i != (totalLoopCount - 1)) {
       i = (totalLoopCount - 1);
     }
     prevResult = result
